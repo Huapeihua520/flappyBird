9278b5078f1c8a60bd028c24d515649a15b78720
Pei <1163994068@qq.com>
2019-10-21 17:26:49 +0800

    第二天结束:   主要内容: 设计背景图,地板,上下水管,小鸟的页面动态显示

--- Main.js
+++ Main.js
@@ -1,17 +1,25 @@
 //程序的主类,用于小游戏过程中数据的初始化,以及点击事件的绑定
 import { ResourceLoader } from "./js/base/ResourceLoader.js";
 import {DataStore} from "./js/base/DataStore.js";
+import { Background } from "./js/runtime/Background.js";
+import { Land } from "./js/runtime/Land.js";
+import { Director } from "./js/Director.js";
+import { UpPipe } from "./js/runtime/UpPipe.js";
+import { Birds } from "./js/player/Birds.js";
 
 export class Main{
   constructor(){
     console.log("游戏开始啦"); 
     //初始化画布
     this.canvas = document.getElementById("game");
+    // this.canvas = wx.createCanvas();
     this.ctx = this.canvas.getContext("2d");
     //初始化资源加载器
-    this.loader = new ResourceLoader();
+    this.loader = new ResourceLoader();    
     //初始化变量池
     this.dataStore = DataStore.getInstance();
+    //初始化导演
+    this.director = Director.getInstance();
     //加载完成后,执行其他的操作
     this.loader.onloaded(map => this.onResourceLoaded(map));
 
@@ -31,8 +39,19 @@ export class Main{
 
     this.init();
   }
-  //游戏初始化
+  //游戏初始化,初始化游戏中的数据,将其保存在变量池中
   init(){
-
+    //模拟画背景图
+    // new Background().draw();
+    // new Land().draw();
+    this.dataStore.set("background",new Background())
+                  .set("land",new Land())
+                  .set("pipes",[])
+                  .set("birds",new Birds())
+    //先创建一组水管
+    this.director.createPipes();
+    
+    //开始运行
+    this.director.run();
   }
 }
